adf79f713ad8644a6dc1d016770edc294f52b2ee
Prudam Dutta <137534667+Psydrite01@users.noreply.github.com>
2026-05-19 21:24:53 +0530

    feat: created screens

---
+++ app/src/main/java/com/example/weathersnap/ui/screens/CreateReportScreen.kt
@@ -0,0 +1,11 @@
+package com.example.weathersnap.ui.screens
+
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.weathersnap.navigation.CreateReportScreenRoute
+
+@Composable
+fun CreateReportScreen(){
+    Text("This is create report page")
+}
---
+++ app/src/main/java/com/example/weathersnap/ui/screens/SavedReportsScreen.kt
@@ -0,0 +1,11 @@
+package com.example.weathersnap.ui.screens
+
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.weathersnap.navigation.CreateReportScreenRoute
+
+@Composable
+fun SavedReportsScreen(){
+    Text("This is saved reports page")
+}
